15cb764f8a851363fe75ef8f9a3aa9f82038d850
initumX <initum.x@gmail.com>
2025-06-28 00:14:52 +0300

    Deleted unused import

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,3 @@
 from .scanner import FileScannerImpl
 from .deduplicator import Deduplicator
 from .models import File, DuplicateGroup, DeduplicationMode
-#from .similar_image_finder import SimilarImageFinder
